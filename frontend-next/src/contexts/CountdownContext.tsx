a7fe42670c444d4259224c5c46b35ea40d360a39
Erick Oliveira <erickoliveirasystem@outlook.com>
2021-03-02 13:35:06 -0300

    Challenges changes

--- frontend-next/src/contexts/CountdownContext.tsx
+++ frontend-next/src/contexts/CountdownContext.tsx
@@ -27,11 +27,11 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(25 * 60);
+  const [time, setTime] = useState(0.1 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
-  const minutes = time / 60;
+  const minutes = Math.floor(time / 60);
   const seconds = time % 60;
 
   function startCoutdown() {
